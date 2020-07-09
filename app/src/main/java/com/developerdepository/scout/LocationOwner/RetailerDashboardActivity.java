29b9a29b04eb799b0909be53337107bf8c7bed6f
2apoorva5 <cu.16bcs1256@gmail.com>
2020-07-10 02:24:33 +0530

    Fifth Commit

--- app/src/main/java/com/developerdepository/scout/LocationOwner/RetailerDashboardActivity.java
+++ app/src/main/java/com/developerdepository/scout/LocationOwner/RetailerDashboardActivity.java
@@ -1,62 +1,104 @@
 package com.developerdepository.scout.LocationOwner;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import com.developerdepository.scout.Databases.SessionManager;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.developerdepository.scout.R;
-import com.ismaeldivita.chipnavigation.ChipNavigationBar;
-
-import java.util.HashMap;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RetailerDashboardActivity extends AppCompatActivity {
 
-    ChipNavigationBar chipNavigationBar;
+    ImageButton dashBoardBtn, profileBtn;
+    ImageView dashboardDot, profileDot;
+    BottomAppBar bottomAppBar;
+    FloatingActionButton fabManage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_dashboard);
 
-        //StatusBar Color
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        getWindow().setStatusBarColor(getResources().getColor(R.color.lightWhite));
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RetailerDashboardFragment()).commit();
+        //Transparent StatusBar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         initViews();
-        bottomMenu();
+        setBottomNavigation();
+
+        dashBoardBtn.performClick();
     }
 
     private void initViews() {
-        chipNavigationBar = findViewById(R.id.retailer_dashboard_bottom_nav);
+        dashBoardBtn = findViewById(R.id.bottom_nav_dashboard);
+        dashboardDot = findViewById(R.id.bottom_nav_dashboard_dot);
+        profileBtn = findViewById(R.id.bottom_nav_profile);
+        profileDot = findViewById(R.id.bottom_nav_profile_dot);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        fabManage = findViewById(R.id.fab_btn_add);
     }
 
-    private void bottomMenu() {
-        chipNavigationBar.setItemSelected(R.id.bottom_nav_dashboard, true);
-        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+    private void setBottomNavigation() {
+        dashBoardBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(int i) {
-                Fragment fragment = null;
-                switch (i) {
-                    case R.id.bottom_nav_dashboard :
-                        fragment = new RetailerDashboardFragment();
-                        break;
-                    case R.id.bottom_nav_manage :
-                        fragment = new RetailerManageFragment();
-                        break;
-                    case R.id.bottom_nav_profile :
-                        fragment = new RetailerProfileFragment();
-                        break;
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            public void onClick(View v) {
+                dashBoardBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                profileBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                dashboardDot.setVisibility(View.VISIBLE);
+                dashboardDot.setImageTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                profileDot.setVisibility(View.GONE);
+                fabManage.setBackgroundTintList(ColorStateList.valueOf(getColor(android.R.color.black)));
+                fabManage.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RetailerDashboardFragment()).commit();
             }
         });
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                dashBoardBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                profileDot.setVisibility(View.VISIBLE);
+                profileDot.setImageTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                dashboardDot.setVisibility(View.GONE);
+                fabManage.setBackgroundTintList(ColorStateList.valueOf(getColor(android.R.color.black)));
+                fabManage.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RetailerProfileFragment()).commit();
+            }
+        });
+
+        fabManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabManage.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                fabManage.setImageTintList(ColorStateList.valueOf(getColor(android.R.color.black)));
+                dashBoardBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                profileBtn.setImageTintList(ColorStateList.valueOf(getColor(R.color.inactiveBottomNavColor)));
+                dashboardDot.setVisibility(View.GONE);
+                profileDot.setVisibility(View.GONE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RetailerManageFragment()).commit();
+            }
+        });
+    }
+
+    public static void setWindowFlag(RetailerDashboardActivity retailerDashboardActivity, final int bits, boolean on) {
+        Window window = retailerDashboardActivity.getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+
+        if (on) {
+            layoutParams.flags |= bits;
+        } else {
+            layoutParams.flags &= ~bits;
+        }
+        window.setAttributes(layoutParams);
     }
 }
